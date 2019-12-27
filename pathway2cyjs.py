32e575f20e7f6db918b392f13a23ebc1207e6e1e
Kozo Nishida <kozo.nishida@gmail.com>
2019-12-27 16:28:22 +0900

    Add wp2cyelements

--- pathway2cyjs.py
+++ pathway2cyjs.py
@@ -32,6 +32,60 @@ def escher2cyelements(escher_json_url):
     cyelements["edges"] = cyedges
     return cyelements
 
+WP_API_BASE = 'https://webservice.wikipathways.org/'
+
+def wp2cyelements(identifier):
+    gpml = requests.get(WP_API_BASE + 'getPathway?pwId=' + identifier + '&format=json').content
+    soup = BeautifulSoup(json.loads(gpml)['pathway']['gpml'], "xml")
+    
+    wpnodes = soup.find_all('DataNode')
+    wpedges = soup.find_all('Interaction')
+
+    cyelements = {}
+    cynodes = []
+    cyedges = []
+    nodeids = []
+
+    for wpn in wpnodes:
+        if wpn['Type'] == "Metabolite":
+            g = wpn.find('Graphics')
+            
+            data = {}
+            data['id'] = wpn['GraphId']
+            nodeids.append(wpn['GraphId'])
+            data['label'] = wpn['TextLabel']
+            data['x'] = float(g['CenterX'])
+            data['y'] = float(g['CenterY'])
+            data['width'] = g['Width']
+            data['height'] = g['Height']
+
+            xref = wpn.find('Xref')
+            if xref is not None:
+                data['database'] = xref['Database']
+                data['xrefID'] = xref['ID']
+                print(data['id'], data['label'], data['database'], data['xrefID'])
+                data['KEGG'] = bridgedbpy.gpml2kegg(xref['Database'], xref['ID'])
+
+        cynode = {"data":data, "position":{"x":float(g["CenterX"]), "y":float(g["CenterY"])}, "selected":"false"}
+        cynodes.append(cynode)
+
+    for wpe in wpedges:
+        data = {}
+        for point in wpe.find_all('Point'):
+            if point.has_attr('GraphRef') and point.has_attr('ArrowHead'):
+                if point['GraphRef'] in nodeids:
+                    data['target'] = point['GraphRef']
+            elif point.has_attr('GraphRef'):
+                if point['GraphRef'] in nodeids:
+                    data['source'] = point['GraphRef']
+        if 'source' in data.keys() and 'target' in data.keys():
+            cyedge = {"data":data}
+            cyedges.append(cyedge)
+
+    cyelements["nodes"] = cynodes
+    cyelements["edges"] = cyedges
+    return cyelements
+
 def cynodes2df(cynodes):
     rows = []
     for cynode in cynodes:
