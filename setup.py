015212e551cd695723df4f7aaf40ecf1f37d3f49
Kozo Nishida <kozo.nishida@gmail.com>
2020-07-17 17:43:09 +0900

    Add lxml to install_requires

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     python_requires='>=3.6',
     author='Kozo Nishida',
     author_email='knishida@riken.jp',
-    install_requires=['requests', 'bridgedbpy', 'beautifulsoup4', 'pandas', 'biopython'],
+    install_requires=['requests', 'bridgedbpy', 'beautifulsoup4', 'pandas', 'biopython', 'lxml'],
     description='Convert Escher, Wikipathways, KEGG pathway data to Cytoscape.js JSON (.cyjs)',
     long_description=long_description,
     long_description_content_type='text/markdown',
